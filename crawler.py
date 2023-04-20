f9960a3c82b8ed61eb6b6e0647c3bb0d06388f0a
FX Wood <fxwood2@gmail.com>
2023-04-19 18:18:27 -0700

    find top 10 words, kind of messy

--- crawler.py
+++ crawler.py
@@ -1,7 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
+from typing import List, Dict
+import re
+
+def filter_strings(strings: List[str]) -> List[str]:
+    """filter out non-words from the soup output"""
+    result = []
+    for string in strings:
+        # this is not great, for example it splits
+        # "Traf-O-Data" and MS-DOS into multiple words
+        # if hyphenated words are not common this has less
+        # impact because we're usually interested in the most
+        # common words
+        words = re.findall(r'\w+', string)
+        result.extend(words)
+    return result
+
+def count_words(words: List[str]) -> Dict:
+    """given a list of words, count instances of each word. case-sensitive"""
+    result = {}
+    for word in words:
+        if word in result:
+            result[word] += 1
+        else:
+            result[word] = 1
+    return result
 
 def crawl():
+    """crawl a given page, for example a wikipedia page, and count words"""
     uri = "https://wikipedia.com/wiki/Microsoft"
     # get uri data
     data = requests.get(uri).text
@@ -27,8 +53,13 @@ def crawl():
                 history_text_contents.append(string)
         next_sibling = next_sibling.find_next_sibling()
 
-    print(history_text_contents)
+    clean_text = filter_strings(history_text_contents)
 
+    count = count_words(clean_text)
+    # https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value
+    sorted_count = {key: value for key, value in sorted(count.items(), key=lambda item: item[1], reverse=True)}
+    export_data = list(sorted_count.items())[:10]
+    print(export_data)
     # count words in html
     # allow excluding words
 
